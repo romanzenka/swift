d138f3d6b335490414b1408d4abeefbe7421fcf4
Roman Zenka <zenka.roman@mayo.edu>
2013-01-03 17:33:40 -0600

    Missed one deprecation.

--- lib/messaging/src/main/java/edu/mayo/mprc/messaging/rmi/OneWayMessenger.java
+++ lib/messaging/src/main/java/edu/mayo/mprc/messaging/rmi/OneWayMessenger.java
@@ -4,13 +4,15 @@ import java.io.Serializable;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+ * @deprecated This would not work in firewalled environment.
+ */
 public interface OneWayMessenger extends Remote, Serializable {
 	/**
 	 * This method is to called on the remote instance of this object.
 	 *
 	 * @param message
 	 * @throws java.rmi.RemoteException
-	 * @deprecated This would not work in firewalled environment.
 	 */
 	void sendMessage(Object message) throws RemoteException;
 }
