ca292091a2c52b6f0b6ea9eac6ff8c47beb0d4a2
Roman Zenka <zenka.roman@mayo.edu>
2013-06-28 23:04:15 -0500

    Fixing the swift web starter.

--- swift/scripts/src/main/resources/swiftWeb.bat
+++ swift/scripts/src/main/resources/swiftWeb.bat
@@ -1,4 +1,4 @@
 @echo off
 set VER=3.5-SNAPSHOT
 echo Starting Swift web server
-java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5121 -cp "bin\swift\*" edu.mayo.mprc.launcher.Launcher --war .\bin\swift\swift-%VER%.war %*
+java -cp "bin\swift\*" edu.mayo.mprc.launcher.Launcher --war .\bin\swift\swift-web-%VER%.war %*
--- swift/scripts/src/main/resources/swiftWeb.sh
+++ swift/scripts/src/main/resources/swiftWeb.sh
@@ -3,4 +3,4 @@
 VER=3.5-SNAPSHOT
 echo Starting Swift web server
 umask 002
-java -Dswift.home=`pwd` -Dlog4j.configuration=file:conf/log4j.properties -cp "bin/swift/*" edu.mayo.mprc.launcher.Launcher --war ./bin/swift/swift-${VER}.war $*
+java -Dswift.home=`pwd` -Dlog4j.configuration=file:conf/log4j.properties -cp "bin/swift/*" edu.mayo.mprc.launcher.Launcher --war ./bin/swift/swift-web-${VER}.war $*
