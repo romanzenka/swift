e86ed7c48600ac82d4c6c3b5a704129148e9fd37
Roman Zenka <zenka.roman@mayo.edu>
2014-03-27 17:38:53 -0500

    Events actually work now. BugzId: 3170

--- swift/web/src/main/java/edu/mayo/mprc/swift/ui/client/widgets/validation/SemiCheckBox.java
+++ swift/web/src/main/java/edu/mayo/mprc/swift/ui/client/widgets/validation/SemiCheckBox.java
@@ -1,9 +1,13 @@
 package edu.mayo.mprc.swift.ui.client.widgets.validation;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.event.shared.GwtEvent;
 import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.event.shared.SimpleEventBus;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Widget;
@@ -21,9 +25,16 @@ import java.util.List;
 public final class SemiCheckBox implements Validatable, IsWidget {
 
 	private CheckBox checkBox;
+	private final EventBus eventBus = new SimpleEventBus();
 
 	public SemiCheckBox() {
 		checkBox = new CheckBox("Semi");
+		checkBox.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				ValueChangeEvent.fire(SemiCheckBox.this, getValue());
+			}
+		});
 	}
 
 	@Override
@@ -72,12 +83,12 @@ public final class SemiCheckBox implements Validatable, IsWidget {
 
 	@Override
 	public HandlerRegistration addValueChangeHandler(final ValueChangeHandler<ClientValue> handler) {
-		return checkBox.addHandler(handler, ValueChangeEvent.getType());
+		return eventBus.addHandler(ValueChangeEvent.getType(), handler);
 	}
 
 	@Override
 	public void fireEvent(final GwtEvent<?> event) {
-		checkBox.fireEvent(event);
+		eventBus.fireEvent(event);
 	}
 
 	@Override
