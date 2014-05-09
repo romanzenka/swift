6d36fc51c8ed3093932f45c045a5259872b65a70
Roman Zenka <zenka.roman@mayo.edu>
2014-05-09 16:28:16 -0500

    MSMSEvalWorker test needs to create a proper msmseval worker. We use the factory to accomplish that.

--- services/msmseval/src/test/java/edu/mayo/mprc/msmseval/MSMSEvalWorkerTest.java
+++ services/msmseval/src/test/java/edu/mayo/mprc/msmseval/MSMSEvalWorkerTest.java
@@ -1,15 +1,24 @@
 package edu.mayo.mprc.msmseval;
 
 import edu.mayo.mprc.MprcException;
+import edu.mayo.mprc.config.DependencyResolver;
 import edu.mayo.mprc.daemon.DaemonWorkerTester;
+import edu.mayo.mprc.daemon.worker.Worker;
+import edu.mayo.mprc.io.mgf.MgfPeakListReaderFactory;
+import edu.mayo.mprc.io.mgf.MzXmlConverter;
+import edu.mayo.mprc.peaklist.PeakListReaderFactory;
+import edu.mayo.mprc.peaklist.PeakListReaders;
 import edu.mayo.mprc.utilities.FileUtilities;
 import edu.mayo.mprc.utilities.TestingUtilities;
+import edu.mayo.mprc.utilities.exceptions.ExceptionUtilities;
 import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class MSMSEvalWorkerTest {
 
@@ -42,13 +51,27 @@ public final class MSMSEvalWorkerTest {
 
 	@Test(dependsOnMethods = {"createTestFiles"}, enabled = true)
 	public void msmsEvalWorkerTest() {
-
-		final MSMSEvalWorker msmsEvalWorker = new MSMSEvalWorker();
 		final File msmsEvalExecutable = MSMSEvalTest.getMsmsEvalExecutable();
 		if (!msmsEvalExecutable.canExecute()) {
 			LOGGER.debug("Cannot test msmsEval, the executable is not present");
 			return;
 		}
+
+		final MSMSEvalWorker.Config config = new MSMSEvalWorker.Config(msmsEvalExecutable.getAbsolutePath(), "orbi," + paramFile.getAbsolutePath());
+		final MSMSEvalWorker.Factory factory = new MSMSEvalWorker.Factory();
+		final List<PeakListReaderFactory> peakListReadersList = new ArrayList<PeakListReaderFactory>();
+		peakListReadersList.add(new MgfPeakListReaderFactory());
+		final PeakListReaders readers = new PeakListReaders();
+		readers.setReaderFactories(peakListReadersList);
+		factory.setConverter(new MzXmlConverter(readers));
+
+		final Worker worker = factory.create(config, new DependencyResolver(null));
+		if (!(worker instanceof MSMSEvalWorker)) {
+			ExceptionUtilities.throwCastException(worker, MSMSEvalWorker.class);
+			return;
+		}
+		final MSMSEvalWorker msmsEvalWorker = (MSMSEvalWorker) worker;
+
 		msmsEvalWorker.setMsmsEvalExecutable(msmsEvalExecutable);
 
 		final DaemonWorkerTester daemonWorkerTester = new DaemonWorkerTester(msmsEvalWorker);
