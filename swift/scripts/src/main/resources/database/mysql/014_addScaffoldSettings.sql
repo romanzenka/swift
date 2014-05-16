686e1962962ad713d0bb795679bcf4e112e25465
Roman Zenka <zenka.roman@mayo.edu>
2014-05-16 13:59:03 -0500

    Fixing typo.

--- swift/scripts/src/main/resources/database/mysql/014_addScaffoldSettings.sql
+++ swift/scripts/src/main/resources/database/mysql/014_addScaffoldSettings.sql
@@ -11,7 +11,7 @@ SET mz_ident_ml_report = 0, high_mass_accuracy_scoring = 0, use_3x_scoring = 0
 WHERE scaffold_settings_id > 0;
 
 ALTER TABLE search_parameters
-ADD COLUMN min_termini_cleavages
+ADD COLUMN min_termini_cleavages INT
 AFTER missed_cleavages;
 
 -- Set everything to fully tryptic (could not do semitryptic searches before)
