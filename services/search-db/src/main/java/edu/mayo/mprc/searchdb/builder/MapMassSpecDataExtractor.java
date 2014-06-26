33bf685818c8dba57bab065f02cf2d0009f8b9e2
Roman Zenka <zenka.roman@mayo.edu>
2014-06-26 10:53:18 -0500

    The extraction has to happen before all the other work, not just during lookups.

--- services/search-db/src/main/java/edu/mayo/mprc/searchdb/builder/MapMassSpecDataExtractor.java
+++ services/search-db/src/main/java/edu/mayo/mprc/searchdb/builder/MapMassSpecDataExtractor.java
@@ -22,14 +22,34 @@ public class MapMassSpecDataExtractor implements MassSpecDataExtractor {
 
 	@Override
 	public TandemMassSpectrometrySample getTandemMassSpectrometrySample(final String biologicalSampleName, final String msmsSampleName) {
-		final RawFileMetaData rawFileMetaData = getMetadata(msmsSampleName);
+		final String cleanedMsmsSampleName = cleanMsmsSampleName(msmsSampleName);
+
+		final RawFileMetaData rawFileMetaData = getMetadata(cleanedMsmsSampleName);
 		if (rawFileMetaData == null) {
 			return null;
 		} else {
 			final TandemMassSpectrometrySample result = rawFileMetaData.parse();
-			sampleMap.put(msmsSampleName, result);
+			sampleMap.put(cleanedMsmsSampleName, result);
 			return result;
 		}
+	}
+
+	/**
+	 * For some strange reason, Scaffold sometimes refers to the sample using parentheses around its name
+	 * We need to strip those
+	 *
+	 * @param msmsSampleName Sample name, possibly with parentheses around
+	 * @return Same name without parentheses
+	 */
+	private String cleanMsmsSampleName(final String msmsSampleName) {
+		final String cleanedMsmsSampleName;
+
+		if (msmsSampleName.startsWith("(") && msmsSampleName.endsWith(")")) {
+			cleanedMsmsSampleName = msmsSampleName.substring(1, msmsSampleName.length() - 1);
+		} else {
+			cleanedMsmsSampleName = msmsSampleName;
+		}
+		return cleanedMsmsSampleName;
 	}
 
 	@Override
@@ -48,14 +68,6 @@ public class MapMassSpecDataExtractor implements MassSpecDataExtractor {
 		final RawFileMetaData metaData = metaDataMap.get(msmsSampleName);
 		if (metaData != null) {
 			return metaData;
-		}
-		// For some strange reason, Scaffold sometimes refers to the sample using parentheses around its name
-		// We need to strip those
-		if (msmsSampleName.startsWith("(") && msmsSampleName.endsWith(")")) {
-			final RawFileMetaData metaNoParens = metaDataMap.get(msmsSampleName.substring(1, msmsSampleName.length() - 1));
-			if (metaNoParens != null) {
-				return metaNoParens;
-			}
 		}
 		if (msmsSampleName.startsWith(MUDPIT_PREFIX)) {
 			final RawFileMetaData mudpitMetaData = metaDataMap.get(msmsSampleName.substring(MUDPIT_PREFIX.length()));
