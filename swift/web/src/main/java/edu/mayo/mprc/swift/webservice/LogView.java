458133a6d8fa944c7b9f2087e409e233f34851ee
Roman Zenka <zenka.roman@mayo.edu>
2014-07-24 22:23:34 -0500

    Better log display.

--- swift/web/src/main/java/edu/mayo/mprc/swift/webservice/LogView.java
+++ swift/web/src/main/java/edu/mayo/mprc/swift/webservice/LogView.java
@@ -5,6 +5,7 @@ import edu.mayo.mprc.swift.db.DatabaseFileTokenFactory;
 import edu.mayo.mprc.swift.db.SwiftDao;
 import edu.mayo.mprc.swift.dbmapping.LogData;
 import edu.mayo.mprc.swift.dbmapping.TaskData;
+import edu.mayo.mprc.utilities.FileUtilities;
 import edu.mayo.mprc.utilities.StringUtilities;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,12 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Sends a log file to the user, eventually with nice formatting etc.
@@ -31,6 +37,9 @@ public final class LogView {
 	@Resource(name = "fileTokenFactory")
 	private DatabaseFileTokenFactory fileTokenFactory;
 
+	private static final Pattern TIME_STAMP = Pattern.compile("^(\\d{1,4}-\\d{1,2}-\\d{1,2}) (\\d{1,2}:\\d{1,2}:\\d{1,2},\\d{1,3}) (.*)");
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,SSS");
+
 	enum LogType {
 		OUTPUT_LOG,
 		ERROR_LOG
@@ -42,17 +51,48 @@ public final class LogView {
 	                          final HttpServletResponse response) {
 
 		final List<LogData> logList = getLogData(taskId);
-		LogType type = getLogTypeFromString(logType);
+		final LogType type = getLogTypeFromString(logType);
 
-		response.setContentType("text/plain");
+		response.setContentType("text/html");
 
 		try {
 			final ServletOutputStream outputStream = response.getOutputStream();
-			outputStream.println("Log files for task " + taskId);
-			outputStream.println();
+			outputStream.println("<!DOCTYPE html>\n" +
+					"<html>\n" +
+					"<head lang=\"en\">\n" +
+					"    <meta charset=\"UTF-8\">\n" +
+					"    <title>Log file for task " + taskId + "</title>\n" +
+					"    <style type=\"text/css\">\n" +
+					"        log {\n" +
+					"            font-family: monospace;\n" +
+					"            font-size: 12px;\n" +
+					"        }\n" +
+					"\n" +
+					"        row {\n" +
+					"            position: relative;\n" +
+					"            display: block;\n" +
+					"        }\n" +
+					"\n" +
+					"        prg {\n" +
+					"            display: block;\n" +
+					"            background-color: #efe;\n" +
+					"            position: absolute;\n" +
+					"            left: 0px;\n" +
+					"            top: 0px;\n" +
+					"            z-index: -1;\n" +
+					"            height: 12px;\n" +
+					"        }\n" +
+					"\n" +
+					"        time {\n" +
+					"            display: inline;\n" +
+					"            font-weight: bold;\n" +
+					"            margin-right: 1em;\n" +
+					"        }\n" +
+					"    </style>\n" +
+					"</head>\n" +
+					"<body>\n");
 
 			for (final LogData data : logList) {
-
 				final File logFile;
 				if (type == LogType.OUTPUT_LOG) {
 					logFile = data.getOutputLog();
@@ -60,16 +100,80 @@ public final class LogView {
 					logFile = data.getErrorLog();
 				}
 
-				outputStream.println(StringUtilities.repeat('-', 80));
-				outputStream.println("Log file " + logFile.getAbsolutePath());
-				outputStream.println();
-				QaReport.streamFileToResponse(response, logFile, false);
+				printLogFile(response, outputStream, logFile);
 			}
+			outputStream.println("</body>\n" +
+					"</html>");
+
 		} catch (final IOException e) {
 			throw new MprcException(e);
 		}
+	}
 
+	private void printLogFile(final HttpServletResponse response, final ServletOutputStream outputStream, final File logFile) throws IOException {
+		outputStream.println(String.format("<h3>Log file %s</h3>", logFile.getAbsolutePath()));
+		outputStream.println("<log>");
 
+		// Long logs are just dumped
+		if (logFile.length() > 5000000) {
+			QaReport.streamFileToResponse(response, logFile, false);
+		} else {
+			final List<String> lines = FileUtilities.readLines(logFile);
+
+			// Establish min and max date
+			Date min = new Date();
+			Date max = null;
+			for (final String line : lines) {
+				final Matcher matcher = TIME_STAMP.matcher(line);
+				if (matcher.matches()) {
+					final String dateTime = matcher.group(1);
+					try {
+						final Date date = DATE_FORMAT.parse(dateTime);
+						if (date.before(min)) {
+							min = date;
+						}
+						if (max == null) {
+							max = date;
+						} else if (max.after(date)) {
+							max = date;
+						}
+					} catch (final ParseException ignore) {
+						// SWALLOWED: We do not care
+					}
+				}
+			}
+			final long minTime = min.getTime();
+			final long maxTime = max != null ? max.getTime() + 1 : new Date().getTime() + 1;
+			long prevTime = minTime;
+			long time = minTime;
+
+			for (final String line : lines) {
+				final Matcher matcher = TIME_STAMP.matcher(line);
+				if (matcher.matches()) {
+					final String dateTime = matcher.group(1) + ' ' + matcher.group(2);
+					final String row = matcher.group(3);
+					Date date = null;
+					try {
+						date = DATE_FORMAT.parse(dateTime);
+						prevTime = time;
+						time = date.getTime();
+					} catch (final ParseException ignore) {
+						// SWALLOWED: We do not care
+					}
+
+					final long width = (int) (1000.0 * (prevTime - minTime) / (maxTime - minTime));
+					outputStream.println("<row>" +
+							"<time title=\"" + dateTime + "\">" + matcher.group(2) + "</time>" +
+							row +
+							"<prg style=\"width: " + width + "px\"></prg>" +
+							"</row>");
+				} else {
+					outputStream.println("<row>" + line + "</row>");
+				}
+
+			}
+		}
+		outputStream.println("</log>");
 	}
 
 	private static LogType getLogTypeFromString(final String logType) {
