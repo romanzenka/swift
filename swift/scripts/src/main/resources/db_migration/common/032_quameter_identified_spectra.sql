0b583dcd5079562401d3bd2159da7741a1434617
Roman Zenka <zenka.roman@mayo.edu>
2014-07-29 23:34:25 -0500

    The id spectra get pre-set to 0.

--- swift/scripts/src/main/resources/db_migration/common/032_quameter_identified_spectra.sql
+++ swift/scripts/src/main/resources/db_migration/common/032_quameter_identified_spectra.sql
@@ -2,6 +2,10 @@ ALTER TABLE quameter_result
 ADD COLUMN identified_spectra INT NULL
 AFTER `p_3`;
 
+UPDATE quameter_result
+SET identified_spectra=0
+WHERE identified_spectra IS NULL;
+
 -- @UNDO
 
 ALTER TABLE quameter_result
