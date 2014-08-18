3333ab3fd785985115944c20a301655092d4fb47
Roman Zenka <zenka.roman@mayo.edu>
2014-08-18 14:40:10 -0500

    Implemented addAnnotations and listAnnotations calls.

--- services/quameter-db/src/main/java/edu/mayo/mprc/quameterdb/dao/QuameterAnnotation.java
+++ services/quameter-db/src/main/java/edu/mayo/mprc/quameterdb/dao/QuameterAnnotation.java
@@ -1,38 +1,89 @@
 package edu.mayo.mprc.quameterdb.dao;
 
-import edu.mayo.mprc.MprcException;
+import com.google.common.base.Objects;
+import edu.mayo.mprc.database.DaoBase;
 import edu.mayo.mprc.database.PersistableBase;
 import org.hibernate.criterion.Criterion;
-import org.springframework.beans.factory.annotation.Autowire;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.hibernate.criterion.Restrictions;
 
 /**
  * @author Raymond Moore
  */
 public class QuameterAnnotation extends PersistableBase {
-    private String metricCode;
-    private Integer quameterResultId;
-    private String text;
+	private String metricCode;
+	private Integer quameterResultId;
+	private String text;
 
+	public QuameterAnnotation() {
+	}
 
-   // public QuameterAnnotation(){}
+	/**
+	 * Helper - useful in case when you know the QuameterColumn
+	 */
+	public QuameterAnnotation(final QuameterResult.QuameterColumn column, final Integer quameterResultId, final String text) {
+		this(column.name(), quameterResultId, text);
+	}
 
-    @Autowired
-    public QuameterAnnotation(String metricCode, Integer quameterResultId, String text) {
-        this.metricCode = metricCode;
-        this.quameterResultId = quameterResultId;
-        this.text = text;
-    }
+	/**
+	 * @param metricCode       Name of the metric. Should correspond to {@link edu.mayo.mprc.quameterdb.dao.QuameterResult.QuameterColumn}
+	 * @param quameterResultId Id of the quameter result this annotation pertains to.
+	 * @param text             Text of the annotation.
+	 */
+	public QuameterAnnotation(final String metricCode, final Integer quameterResultId, final String text) {
+		this.metricCode = metricCode;
+		this.quameterResultId = quameterResultId;
+		this.text = text;
+	}
 
-    public QuameterAnnotation(String s) {
-    }
+	public String getMetricCode() {
+		return metricCode;
+	}
 
-    @Override
-    public Criterion getEqualityCriteria() {
-        throw new MprcException("EqualityCriteria Not Implemented");
-    }
+	public void setMetricCode(final String metricCode) {
+		this.metricCode = metricCode;
+	}
 
-    public String prettyPrint(){
-        return "Code: "+metricCode+" ResultId: "+quameterResultId+"\nText: "+text;
-    }
+	public Integer getQuameterResultId() {
+		return quameterResultId;
+	}
+
+	public void setQuameterResultId(final Integer quameterResultId) {
+		this.quameterResultId = quameterResultId;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(final String text) {
+		this.text = text;
+	}
+
+	@Override
+	public Criterion getEqualityCriteria() {
+		return Restrictions.conjunction()
+				.add(DaoBase.nullSafeEq("metricCode", getMetricCode()))
+				.add(DaoBase.nullSafeEq("quameterResultId", getQuameterResultId()));
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(metricCode, quameterResultId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null || getClass() != obj.getClass()) {
+			return false;
+		}
+		final QuameterAnnotation other = (QuameterAnnotation) obj;
+		return Objects.equal(this.metricCode, other.metricCode) && Objects.equal(this.quameterResultId, other.quameterResultId);
+	}
+
+	public String prettyPrint() {
+		return "Code: " + metricCode + " ResultId: " + quameterResultId + "\nText: " + text;
+	}
 }
