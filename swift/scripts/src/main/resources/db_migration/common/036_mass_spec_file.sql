9605f867adc7f17d4db421b9d57c2d5e8ab28a44
Roman Zenka <zenka.roman@mayo.edu>
2014-08-28 14:52:35 -0500

    Issue with the view.

--- swift/scripts/src/main/resources/db_migration/common/036_mass_spec_file.sql
+++ swift/scripts/src/main/resources/db_migration/common/036_mass_spec_file.sql
@@ -7,6 +7,34 @@ ALTER TABLE tandem_mass_spec_sample
 ALTER TABLE tandem_mass_spec_sample
   ADD UNIQUE KEY `uniqueness` (`sample_file`, `last_modified`);
 
+CREATE OR REPLACE VIEW analysis_to_proteins AS
+select
+  t.title,
+  re.transaction_id,
+  a.analysis_id,
+  bs.biological_sample_id,
+  bs.category,
+  bs.sample_name,
+  ts.tandem_mass_spec_sample_id,
+  ts.sample_file,     -- file -> sample_file
+  sr.protein_group_list_id
+from analysis as a,
+  biological_sample_list_members bslm,
+  biological_sample as bs,
+  search_result_list_members as srlm,
+  search_result as sr,
+  tandem_mass_spec_sample as ts,
+  report as re,
+  latest_functional_transaction as t
+where
+  a.biological_sample_list_id = bslm.biological_sample_list_id
+and bslm.biological_sample_id = bs.biological_sample_id
+and bs.search_result_list_id = srlm.search_result_list_id
+and srlm.search_result_id = sr.search_result_id
+and sr.tandem_mass_spec_sample_id = ts.tandem_mass_spec_sample_id
+and re.analysis_id = a.analysis_id
+and t.transaction_id = re.transaction_id;
+
 -- @UNDO
 
 ALTER TABLE tandem_mass_spec_sample
@@ -17,3 +45,31 @@ CHANGE COLUMN sample_file `file` VARCHAR(255) NOT NULL;
 
 ALTER TABLE tandem_mass_spec_sample
   ADD UNIQUE KEY `uniqueness` (`file`, `last_modified`);
+
+CREATE OR REPLACE VIEW analysis_to_proteins AS
+select
+  t.title,
+  re.transaction_id,
+  a.analysis_id,
+  bs.biological_sample_id,
+  bs.category,
+  bs.sample_name,
+  ts.tandem_mass_spec_sample_id,
+  ts.file,     -- sample_file -> file
+  sr.protein_group_list_id
+from analysis as a,
+  biological_sample_list_members bslm,
+  biological_sample as bs,
+  search_result_list_members as srlm,
+  search_result as sr,
+  tandem_mass_spec_sample as ts,
+  report as re,
+  latest_functional_transaction as t
+where
+  a.biological_sample_list_id = bslm.biological_sample_list_id
+and bslm.biological_sample_id = bs.biological_sample_id
+and bs.search_result_list_id = srlm.search_result_list_id
+and srlm.search_result_id = sr.search_result_id
+and sr.tandem_mass_spec_sample_id = ts.tandem_mass_spec_sample_id
+and re.analysis_id = a.analysis_id
+and t.transaction_id = re.transaction_id;
