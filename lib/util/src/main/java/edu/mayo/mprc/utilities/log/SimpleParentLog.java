fabab95719f6e6ea966b80b68331d78b7279a2c5
Roman Zenka <zenka.roman@mayo.edu>
2014-09-09 12:49:31 -0500

    The simple log does not create files (that was done just to test file leaks).

--- lib/util/src/main/java/edu/mayo/mprc/utilities/log/SimpleParentLog.java
+++ lib/util/src/main/java/edu/mayo/mprc/utilities/log/SimpleParentLog.java
@@ -1,12 +1,7 @@
 package edu.mayo.mprc.utilities.log;
 
-import edu.mayo.mprc.MprcException;
-import edu.mayo.mprc.utilities.FileUtilities;
 import org.apache.log4j.Logger;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.UUID;
 
 /**
@@ -20,8 +15,6 @@ public final class SimpleParentLog implements ChildLog {
 	private static final long serialVersionUID = -291080842199803090L;
 
 	private final UUID id = UUID.randomUUID();
-	private File file;
-	private FileWriter writer;
 
 	public SimpleParentLog() {
 
@@ -45,19 +38,11 @@ public final class SimpleParentLog implements ChildLog {
 	@Override
 	public void startLogging() {
 		LOGGER.debug(id.toString() + " started logging");
-		try {
-			file = File.createTempFile("test", ".log");
-			writer = new FileWriter(file, true);
-		} catch (IOException e) {
-			throw new MprcException(e);
-		}
 	}
 
 	@Override
 	public void stopLogging() {
 		LOGGER.debug(id.toString() + " stopped logging");
-		FileUtilities.closeQuietly(writer);
-		FileUtilities.cleanupTempFile(file);
 	}
 
 	@Override
