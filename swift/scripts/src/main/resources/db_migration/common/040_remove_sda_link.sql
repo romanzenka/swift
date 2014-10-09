811c18c35840f7e2f06d2d77c58fa28c75cdaaf5
Roman Zenka <zenka.roman@mayo.edu>
2014-10-08 21:31:18 -0500

    Minor fixes to migration scripts.

--- swift/scripts/src/main/resources/db_migration/common/040_remove_sda_link.sql
+++ swift/scripts/src/main/resources/db_migration/common/040_remove_sda_link.sql
@@ -1,6 +1,6 @@
-ALTER TABLE `atlas_migration`.`curation_step_new_db`
+ALTER TABLE `curation_step_new_db`
 DROP FOREIGN KEY `curation_step_new_db_ibfk_1`;
-ALTER TABLE `atlas_migration`.`curation_step_new_db`
+ALTER TABLE `curation_step_new_db`
 DROP COLUMN `source_db`,
 DROP INDEX `curation_step_new_db_ibfk_1` ;
 
