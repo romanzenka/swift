140a68fca591805309dc09e7a12647da7f3dd77f
Roman Zenka <zenka.roman@mayo.edu>
2014-11-11 09:23:38 -0600

    All file columns were bumped to at least 500 characters.

--- swift/scripts/src/main/resources/db_migration/common/055_input_file_path.sql
+++ swift/scripts/src/main/resources/db_migration/common/055_input_file_path.sql
@@ -1,3 +1,8 @@
 ALTER TABLE file_search MODIFY COLUMN input_file VARCHAR(500);
+ALTER TABLE tandem_mass_spec_sample MODIFY COLUMN sample_file VARCHAR(500);
+ALTER TABLE swift_search_definition MODIFY COLUMN output_folder VARCHAR(500);
+ALTER TABLE log MODIFY COLUMN out_log VARCHAR(500), MODIFY COLUMN err_log VARCHAR(500);
+ALTER TABLE source_database_archive MODIFY COLUMN source_path VARCHAR(500);
+ALTER TABLE curation MODIFY COLUMN curated_filename VARCHAR(500);
 
 -- @UNDO
