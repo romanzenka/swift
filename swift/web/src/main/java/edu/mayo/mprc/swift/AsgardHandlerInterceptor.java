b66df94c34ce75b92c228aac2b0d5dd84d79feee
Roman Zenka <zenka.roman@mayo.edu>
2015-01-27 12:48:52 -0600

    Supporting Asgard dashboard

---
+++ swift/web/src/main/java/edu/mayo/mprc/swift/AsgardHandlerInterceptor.java
@@ -0,0 +1,24 @@
+package edu.mayo.mprc.swift;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Roman Zenka
+ */
+public final class AsgardHandlerInterceptor extends HandlerInterceptorAdapter {
+	private final static String ASGARD_PREFIX = "http://asgard";
+
+	@Override
+	public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) throws Exception {
+		if (request.getRequestURL().toString().startsWith(ASGARD_PREFIX)) {
+
+			request.getRequestDispatcher("/dashboard.jsp").forward(request, response);
+			return false;
+		} else {
+			return super.preHandle(request, response, handler);
+		}
+	}
+}
