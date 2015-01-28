465c990a0fd955e1c7dba4b99da8e19843d11eec
Roman Zenka <zenka.roman@mayo.edu>
2015-01-28 14:40:30 -0600

    More hacking.

--- swift/web/src/main/java/edu/mayo/mprc/swift/AsgardHandlerInterceptor.java
+++ swift/web/src/main/java/edu/mayo/mprc/swift/AsgardHandlerInterceptor.java
@@ -20,7 +20,7 @@ public final class AsgardHandlerInterceptor extends HandlerInterceptorAdapter {
 	public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) throws Exception {
 		if (request.getRequestURL().toString().startsWith(ASGARD_PREFIX) && request.getAttribute(ASGARD_HANDLED) == null) {
 			request.setAttribute(ASGARD_HANDLED, true);
-			if (request.getRequestURL().toString().contains("images")) {
+			if (request.getRequestURL().toString().contains("images") || request.getRequestURL().toString().contains("css")) {
 				return super.preHandle(request, response, handler);
 			} else {
 				request.getRequestDispatcher("/dashboard").forward(request, response);
