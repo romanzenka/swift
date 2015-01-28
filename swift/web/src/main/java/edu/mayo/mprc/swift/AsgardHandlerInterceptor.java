a744ea20c8fdc7201b5952a429957454ef19fb01
Roman Zenka <zenka.roman@mayo.edu>
2015-01-28 14:27:58 -0600

    Asgard handling fix.

--- swift/web/src/main/java/edu/mayo/mprc/swift/AsgardHandlerInterceptor.java
+++ swift/web/src/main/java/edu/mayo/mprc/swift/AsgardHandlerInterceptor.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 public final class AsgardHandlerInterceptor extends HandlerInterceptorAdapter {
 	private final static String ASGARD_PREFIX = "http://asgard";
+	public static final String ASGARD_HANDLED = "asgardHandled";
 
 	public AsgardHandlerInterceptor() {
 		int i = 0;
@@ -17,8 +18,8 @@ public final class AsgardHandlerInterceptor extends HandlerInterceptorAdapter {
 
 	@Override
 	public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) throws Exception {
-		if (request.getRequestURL().toString().startsWith(ASGARD_PREFIX)) {
-
+		if (request.getRequestURL().toString().startsWith(ASGARD_PREFIX) && request.getAttribute(ASGARD_HANDLED) != null) {
+			request.setAttribute(ASGARD_HANDLED, true);
 			request.getRequestDispatcher("/dashboard").forward(request, response);
 			return false;
 		} else {
