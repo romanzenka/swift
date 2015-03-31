883ec6e3c81edcea2c0385d0d461d2c40b422514
Roman Zenka <zenka.roman@mayo.edu>
2015-03-31 10:57:54 -0500

    Speeding up the sequence load step.

--- services/fasta-db/src/main/java/edu/mayo/mprc/fastadb/SequenceBulkLoader.java
+++ services/fasta-db/src/main/java/edu/mayo/mprc/fastadb/SequenceBulkLoader.java
@@ -4,15 +4,20 @@ import edu.mayo.mprc.database.SessionProvider;
 import edu.mayo.mprc.database.bulk.BulkLoadJobStarter;
 import edu.mayo.mprc.database.bulk.BulkLoader;
 import edu.mayo.mprc.database.bulk.TempKey;
+import org.hibernate.dialect.SQLServerDialect;
 
 /**
  * @author Roman Zenka
  */
 public final class SequenceBulkLoader extends BulkLoader<Sequence> {
 	private final String tableName;
+	// When true, we try to optimize the sequence queries on MSSQL server
+	// We expect a calculated sequence_start column that is indexed on the table
+	private final boolean msSqlOptimization;
 
 	public SequenceBulkLoader(final BulkLoadJobStarter jobStarter, final SessionProvider sessionProvider, final String tableName) {
 		super(jobStarter, sessionProvider);
+		msSqlOptimization = sessionProvider.getDialect() instanceof SQLServerDialect;
 		this.tableName = tableName;
 	}
 
@@ -28,6 +33,9 @@ public final class SequenceBulkLoader extends BulkLoader<Sequence> {
 
 	@Override
 	public String getEqualityString() {
+		if (msSqlOptimization) {
+			return "<t>.sequence = <s>.sequence AND LEFT(<t>.sequence, 200) = <s>.sequence_start";
+		}
 		return "<t>.sequence = <s>.sequence";
 	}
 
