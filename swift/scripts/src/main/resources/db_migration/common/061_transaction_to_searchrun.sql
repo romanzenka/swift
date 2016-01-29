b818b7c4d190003ad31308da9816df8903d3621d
Roman Zenka <zenka.roman@mayo.edu>
2016-01-28 19:11:29 -0600

    Better migration script

--- swift/scripts/src/main/resources/db_migration/common/061_transaction_to_searchrun.sql
+++ swift/scripts/src/main/resources/db_migration/common/061_transaction_to_searchrun.sql
@@ -1,4 +1,16 @@
 ALTER TABLE `transaction`
 CHANGE COLUMN `transaction_id` `search_run_id` INT(11) NOT NULL AUTO_INCREMENT, RENAME TO `search_run`;
 
+ALTER TABLE report CHANGE COLUMN transaction_id search_run_id INT;
+
+ALTER TABLE `task`
+DROP FOREIGN KEY `task_ibfk_2`;
+ALTER TABLE `task`
+CHANGE COLUMN `transaction_id` `search_run_id` INT(11) NULL DEFAULT NULL;
+ALTER TABLE `task`
+ADD CONSTRAINT `task_ibfk_2`
+FOREIGN KEY (`search_run_id`)
+REFERENCES `search_run` (`search_run_id`);
+
+
 -- @UNDO
