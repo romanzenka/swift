63aabd5c9077810f4e38677df22de3971232a8d2
Roman Zenka <zenka.roman@mayo.edu>
2016-02-17 09:45:11 -0600

    Cleaning up migration scripts

--- swift/scripts/src/main/resources/db_migration/atlas3.8/005_multipleConvertors.sql
+++ swift/scripts/src/main/resources/db_migration/atlas3.8/005_multipleConvertors.sql
@@ -1,6 +1,10 @@
 ALTER TABLE `extract_msn_settings` ADD COLUMN `command` VARCHAR(50) NULL DEFAULT NULL
 AFTER `command_line_switches`;
 
+ALTER TABLE `extract_msn_settings`
+DROP INDEX `command_line_switches`,
+ADD UNIQUE INDEX `command_line_switches` (`command_line_switches` ASC, `command` ASC);
+
 UPDATE `extract_msn_settings`
 SET command="extract_msn"
 WHERE 1 = 1;
--- swift/scripts/src/main/resources/db_migration/common/049_transaction.sql
+++ swift/scripts/src/main/resources/db_migration/common/049_transaction.sql
@@ -1,9 +1,9 @@
 ALTER TABLE `transaction`
   DROP COLUMN xml_def_file;
 
-ALTER TABLE `transaction`
-        ADD KEY `transaction_ibfk_2` (`swift_search`),
-        ADD CONSTRAINT `transaction_ibfk_2` FOREIGN KEY (`swift_search`) REFERENCES `swift_search_definition` (`swift_search_definition_id`);
+# ALTER TABLE `transaction`
+#         ADD KEY `transaction_ibfk_2` (`swift_search`),
+#         ADD CONSTRAINT `transaction_ibfk_2` FOREIGN KEY (`swift_search`) REFERENCES `swift_search_definition` (`swift_search_definition_id`);
 
 -- @UNDO
 
